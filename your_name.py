46db2855d4e7a09787d85802512d2f0724ec4e7c
zizobimy1999 <155669416+zizobimy1999@users.noreply.github.com>
2024-02-11 02:33:22 -0500

    Delete your_name.py

--- your_name.py
+++
@@ -1,9 +0,0 @@
-def main():
-    name = input ("What is your name ?" )
-    print ("we want to know if you like programming!")
-    answer = input ("Do you like programming "+ name + "?" )
-    print('Great! You said "'+ answer + '"?')
-    print ("let's learn some Python today")
-
-if __name__ == "__main__":
-        main()
